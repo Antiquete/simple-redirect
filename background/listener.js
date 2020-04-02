3e6b9f4917357df683e03d5a44cf2cf5e587925b
Hari Saksena <hari.mail@protonmail.ch>
2020-04-03 04:16:20 +0530

    Add background script

--- background/listener.js
+++ background/listener.js
@@ -16,3 +16,74 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Simple Redirect.  If not, see <http://www.gnu.org/licenses/>.
+
+Rule = function(source, target, isRegex, isEnabled = true) {
+  this.source = source;
+  this.target = target;
+  this.isRegex = isRegex;
+  this.isEnabled = isEnabled;
+};
+
+var Redirects = [];
+browser.storage.local.get().then(function(value) {
+  if (Array.isArray(value.redirects)) Redirects = value.redirects;
+});
+
+function saveRedirects() {
+  browser.storage.local.set({ redirects: Redirects });
+}
+
+function updateRedirect(source, target) {
+  for (const [i, r] of Redirects.entries()) {
+    if (r.source == source) {
+      r.target = target;
+    }
+  }
+  saveRedirects();
+}
+
+function setRedirect(source, target, isRegex = false) {
+  Redirects.push(new Rule(source, target, isRegex));
+  saveRedirects();
+}
+
+function unsetRedirect(source) {
+  for (const [i, r] of Redirects.entries()) {
+    if (r.source === source) {
+      Redirects.splice(i, 1);
+    }
+  }
+  saveRedirects();
+}
+
+function sendNotification(message) {
+  browser.notifications.create({
+    type: "basic",
+    iconUrl: browser.extension.getURL("icons/icon.svg.png"),
+    title: "Simple Redirect",
+    message: message
+  });
+}
+
+function redirect(e) {
+  if ((e.originUrl || "").indexOf("moz-extension://") === 0) return e;
+
+  // Loop through all redirect rules
+  for (const [i, r] of Redirects.entries()) {
+    // Check if only source exists inside url (Avoid cases like, old.old.old......old.reddit.com)
+    if (!e.url.includes(r.target) && e.url.includes(r.source)) {
+      // Set redirectUrl to target
+      e.redirectUrl = e.url.replace(new RegExp(r.source, "gi"), r.target);
+      sendNotification(`${e.url} -> Redirected to: ${e.redirectUrl}`);
+      console.log(`Changing Request From: ${e.url} To: ${e.redirectUrl}`);
+      break;
+    }
+  }
+
+  // Return modified/non-modified details;
+  return e;
+}
+
+const filters = { urls: ["<all_urls>"] };
+
+browser.webRequest.onBeforeRequest.addListener(redirect, filters, ["blocking"]);
