2990b67372756dc0a636f161e62ea857a5a5f4df
Hari Saksena <hari.mail@protonmail.ch>
2020-04-12 20:42:30 +0530

    Add conditionals for options_ui in background script

--- src/ui/settings.js
+++ src/ui/settings.js
@@ -2,17 +2,25 @@
 import $ from "jquery";
 
 browser.storage.local.get().then(function (value) {
-  if (value.enableNotifications) {
-    $("#input-notification").prop("checked", true);
+  if (value.disableNotifications === true) {
+    $("#input-notifications").removeProp("checked");
   }
-  if (value.enableDeepRedirection) {
-    $("#input-deep-redirection").prop("checked", true);
+  if (value.disableDeepRedirection === true) {
+    $("#input-deep-redirection").removeProp("checked");
   }
 
-  $("#input-notification").change(function () {
-    browser.storage.local.set({ enableNotifications: this.checked });
+  $("#input-notifications").change(function () {
+    let state = this.checked;
+    browser.storage.local.set({ disableNotifications: !state });
+    browser.runtime.getBackgroundPage().then(function (page) {
+      page.allowNotifications = state;
+    });
   });
   $("#input-deep-redirection").change(function () {
-    browser.storage.local.set({ enableDeepRedirection: this.checked });
+    let state = this.checked;
+    browser.storage.local.set({ disableDeepRedirection: !state });
+    browser.runtime.getBackgroundPage().then(function (page) {
+      page.allowDeepRedirects = state;
+    });
   });
 });
